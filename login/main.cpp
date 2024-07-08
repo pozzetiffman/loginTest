04a813c51ebd03f8b6b48b21dbcdfc9c20d43ea2
Mykyta Lvov <mykytalvov@gmail.com>
2024-07-08 15:49:59 +0300

    Initial Commit

---
+++ login/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  login
+//
+//  Created by admin on 8.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
